f78fec9df47f70ec5eed0add72cd7c7d80605e78
Alvarodevs <alvaro.develops@gmail.com>
2021-01-19 22:38:56 +0000

    Final commit

--- src/js/component/trafficlight.js
+++ src/js/component/trafficlight.js
@@ -1,21 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 export function Trafficlight() {
+	const [light, setLight] = useState("");
+	//console.log(light);
+
+	var redLightSelection = "";
+	if (light == "red") redLightSelection = "selected";
+
+	var orangeLightSelection = "";
+	if (light == "orange") orangeLightSelection = "selected";
+
+	var greenLightSelection = "";
+	if (light == "green") greenLightSelection = "selected";
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="maincontainer container">
+			<div
+				className={"red circle " + redLightSelection}
+				onClick={() => setLight("red")}
+			/>
+			<div
+				className={"orange circle " + orangeLightSelection}
+				onClick={() => setLight("orange")}
+			/>
+			<div
+				className={"green circle " + greenLightSelection}
+				onClick={() => setLight("green")}
+			/>
 		</div>
 	);
 }
